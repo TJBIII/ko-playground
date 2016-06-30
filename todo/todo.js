e3d9e41fd0953dcaedcc2993c92c94ab68aea2b1
Thomas Buida <thomasbuida@gmail.com>
2016-06-29 22:07:54 -0500

    make todo constructor and add completed checkbox

--- todo/todo.js
+++ todo/todo.js
@@ -1,17 +1,35 @@
-var SimpleListModel = function(items) {
-  this.todos = ko.observableArray(items);
+"use strict";
 
-  this.itemToAdd = ko.observable("");
-
-  this.addItem = function () {
-    if (this.itemToAdd() != ""){
-      this.todos.push(this.itemToAdd());
-      this.itemToAdd("");
-    }
-  }.bind(this);
-
-  this.removeItem = function(item) { this.todos.remove(item) }.bind(this);
+function Item(data) {
+  this.title = ko.observable(data.title);
+  this.isDone = ko.observable(data.isDone);
 }
 
+var TodoViewModel = function(items) {
+  var self = this;
 
-ko.applyBindings(new SimpleListModel(["Watch Silicon Valley on HBO", "Go climbing", "Read 'The Pragmatic Programmer'"]));
+  self.todos = ko.observableArray(items);
+  self.itemToAdd = ko.observable("");
+
+  self.incompleteTodos = ko.computed(function() {
+    return ko.utils.arrayFilter(self.todos(), task => !task.isDone() );
+  })
+
+  self.addItem = function () {
+    if (self.itemToAdd() != ""){
+      //add the new item
+      self.todos.push(new Item({
+        title: self.itemToAdd(),
+        isDone: false
+      }));
+      //reset the input field
+      self.itemToAdd("");
+    }
+  }
+
+  self.removeItem = item => self.todos.remove(item);
+}
+
+var someItems = [{title: "Watch Silicon Valley on HBO", isDone:false}, {title: "Go climbing", isDone: false}, {title: "Read 'The Pragmatic Programmer'", isDone: false}].map( item => new Item(item));
+
+ko.applyBindings(new TodoViewModel(someItems));
